f3980c00cb6bbb8b8ce420214e052fe2d262b729
Asmae103 <asmae.zaloufi.pro@gmail.com>
2024-11-07 22:26:12 +0100

    Update index.jsx

--- src/index/index.jsx
+++ src/index/index.jsx
@@ -3,12 +3,25 @@ import { useNavigate } from 'react-router-dom'
 import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
 import './index1.css'
+import Connexion from '../composant/connecter'
 
 
 function App() {
   const [count, setCount] = useState(0)
   const  navigate = useNavigate();
-  const errlogin = alert("Erreur login or password");
+  const [error, setError] = useState(false);
+  const[login , SetLogin] = useState('aribiA');
+  const[password , SetPassword] = useState('aaaa');
+
+  function connextion(){
+    e.preventDefault();
+
+    if(login== "aribiA" && password=="aaaa" ){
+      console.log("Connexion reussie");
+    }else{
+      setError(false);
+    }
+  }
   return (
     <>
       <form> 
