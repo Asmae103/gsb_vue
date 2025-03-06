558f300be80d93750d99b28c082fd587fe5457d2
Asmae Zaloufi <asmae.zaloufi.pro@gmail.com>
2025-03-06 12:16:38 +0100

    V14

--- src/pages/accueil/Medecins.jsx
+++ src/pages/accueil/Medecins.jsx
@@ -2,6 +2,7 @@ import { useNavigate, useLocation, useOutletContext } from 'react-router-dom'
 //import reactLogo from '../assets/react.svg'
 import { useState, useEffect } from 'react';
 import './Medecins.css';
+import { getMedecins } from '../../api/medcinapi.jsx';
 import Accueil from './Accueil.jsx';
 
 function Medecins(){
@@ -14,22 +15,32 @@ function Medecins(){
    const [listeMedecins, setListeMedecins] = useState([]); //liste qui va contenir les medecins trouvés 
    const [ version, setVersion] = useState(0); //etat qui permet de forcer le rafraichissement du contenu
    console.log(state);
-  function charger(){
-      if(nomMedecin != null){
+  function charger(e) {
+   const valeursaisie = e.target.value;
+   setNomMedecin(valeursaisie);
+   if (medecin?.nom === valeursaisie) {
+      return medecin.nom + medecin.prenom; 
+   }
+   /*
+       if(nomMedecin != null){
            if(medecin.nom== nomMedecin){
               return medecin.nom + medecin.prenom; 
-           }
-      }
-  }
+            }*/
+   }
+  
   /**
   * Fonction qui va se declencher lorsqu'un medecin est selectionné dans la liste
-  * @param {JSON} leMedecin 
+  * @param {JSON} leMedecin // utilisée dans les commentaires JSDoc pour documenter une fonction JavaScript
   */
    
    
   function selectMedecin(leMedecin){
-  //    console.log(leMedecin.nom);
-     
+     console.log(leMedecin.nom);
+     setMedecin (leMedecin);
+     setNomMedecin (leMedecin.nom + leMedecin.prenom);
+     if(medecin){ // ou medecin ===true  ; === compare les valeurs et les types
+       setListeVisible(false); 
+     }
      return leMedecin;
   }
 
@@ -56,12 +67,13 @@ function Medecins(){
       
 
        <label> Recherche un medecin</label>
-       <input  type ="text"  value={(nomMedecin)}  onChange= {charger} />
+       <br/>
+       <input  type ="text"  value={nomMedecin}  onChange= {charger} />
        
        <ul>
          {listeVisible && listeMedecins.map((medecin ) =>
          <li key={medecin.id}
-            onClick = { () => selectMedecin(leMedecin) } >
+            onClick = { () => selectMedecin(medecin) } >
             { medecin.nom } {medecin.prenom}
          </li>
          )}
